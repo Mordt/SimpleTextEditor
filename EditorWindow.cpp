ed14ec24db05f189da3ff1abef5614d61ac878b9
Carlos Quintero <carloshq@vipglobal.com.au>
2022-09-01 15:55:47 +1000

    definitions for editorwindow class

---
+++ EditorWindow.cpp
@@ -0,0 +1,15 @@
+class EditorWindow : public Fl_Double_Window {
+    public:
+        EditorWindow(int w, int h, const char* t);
+        ~EditorWindow();
+
+        Fl_Window           *replace_dlg;
+        Fl_Input            *replace_find;
+        Fl_Input            *replace_with;
+        Fl_Button           *replace_all;
+        Fl_Return_Button    *replace_next;
+        Fl_Button           *replace_cancel;
+
+        Fl_Text_Editor      *editor;
+        char                search[256]
+};
